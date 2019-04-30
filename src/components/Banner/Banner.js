258799c433c4f2b8ab8e1fcfc40245302f17e0b5
Arthur <Kingarthurla@mail.ru>
2019-04-29 20:57:38 -0700

    update

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,7 @@ import './Banner.css';
 
 const Banner = () => (
   <div className="banner text-center d-flex align-items-center justify-content-center">
-    <p className="m-0">Select a player to build a team, but don't click on any more than once!</p>
+    <p className="m-0">Select a player to build a team, but don't click on any player more than once!</p>
   </div>
 );
 
